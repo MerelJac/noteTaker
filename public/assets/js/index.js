2dbc470dab9f9ab98f629cba0836274b242fc094
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-17 12:22:51 -0700

    API deployed on /api/notes page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,28 +1,25 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
-
 const path = require('path')
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+const db = require('../../../db/db.json')
 
 const filePath = path.join(__dirname, '../../../public', 'index.html');
 const publicFolder = path.join(__dirname, '../../../public')
 
 // calls the index.js file
 app.use(express.static(publicFolder));
+// middleware 
+app.use(express.json());
 
 
 app.get('/notes', (req, res) => {
-  const notesPath = path.join(publicFolder, 'notes.html');
   res.sendFile(path.join(publicFolder, 'notes.html'));
-  console.log('this is the notes page"')
+})
 
-
-
+app.get('/api/notes', (req, res) => {
+  res.json(db)
 })
 
 // call server
